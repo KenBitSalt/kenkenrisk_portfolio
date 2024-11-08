529097cb127521b833cb05f27b3a6327b6eb5baa
KenBitSalt <schen0420@gmail.com>
2024-11-08 12:27:05 -0500

    Update PCA_portfolio.py

--- pages/PCA_portfolio.py
+++ pages/PCA_portfolio.py
@@ -12,18 +12,15 @@ st.session_state.disabled = False
 col1, col2 = st.columns(2)
 
 with col1:
-    st.text_input(
-        "Placeholder for the other text input widget",
-        "This is a placeholder",
-        key="placeholder",
+    st.radio(
+        "Select index as benchmark",
+        key="index",
+        options=["CSI-500", "SP500"],
     )
-    values = st.slider("Select a range of values", 0.0, 100.0, (25.0, 75.0))
-    age = st.slider("Set target month of option", 0, 130, 25)
-    st.write("I'm ", age, "years old")
-    st.write("Values:", values)
 
 with col2:
-    uploaded_file = st.file_uploader("Upload a csv file containing optimized value for each stock")
+    st.markdown("Upload a csv file containing selectable pool and optimized value (preference) for each item")
+    uploaded_file = st.file_uploader("Must Cols: [stock_id], [preference]: optimzation target")
     if uploaded_file is not None:
         # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
