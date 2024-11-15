5947dbac47afc67140ef93d3b7981640682cd9eb
KenBitSalt <schen0420@gmail.com>
2024-11-14 19:07:03 -0500

    1

--- pages/PCA_portfolio.py
+++ pages/PCA_portfolio.py
@@ -9,6 +9,7 @@ st.sidebar.markdown("# pca portfolio")
 st.session_state.visibility = "visible"
 st.session_state.disabled = False
 st.session_state.index = "SP500"
+use_preset = False
 
 col1, col2 = st.columns(2)
 
@@ -19,19 +20,31 @@ with col1:
         options=["CSI-500", "SP500"],
     )
 
+    st.radio(
+        "Maximize or minimize optimization objective",
+        key="direction",
+        options=["Maximize", "Minimize"],
+    )
+
 with col2:
     st.markdown("Upload a csv file containing selectable pool and optimized value (preference) for each item")
-    uploaded_file = st.file_uploader("Must Cols: [stock_id]: ticker, [preference]: optimzation target")
+    uploaded_file = st.file_uploader("Must Cols: [stock_id]: ticker, [objective]: optimzation objective")
     if uploaded_file is not None:
         # To read file as bytes:
         bytes_data = uploaded_file.getvalue()
         # To convert to a string based IO:
         # Can be used wherever a "file-like" object is accepted:
         dataframe = pd.read_csv(uploaded_file)
-
         st.dataframe(
             dataframe,
             hide_index=False,
         )
+
+    use_preset = st.checkbox("Use preset pool")
+
+    if use_preset:
+        st.write("Using Preset pool")
+    if use_preset:
+        st.write("Using User-Designated Pool")
 
     
