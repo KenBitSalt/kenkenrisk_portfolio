a35bc7933a4812de22ca289b129d29afc1097a8a
KenBitSalt <schen0420@gmail.com>
2025-06-29 11:21:09 +0800

    Update path_dependent_exotics.py

--- pages/path_dependent_exotics.py
+++ pages/path_dependent_exotics.py
@@ -25,10 +25,9 @@ with col1:
     simulation_times = int(st.slider("Set simulation times: ", 50, 1000, 1))
     steps = int(st.slider("Set simulation steps: ", 12, 365, 1))
     engine = MonteCarloEngine(S0=100, r=0.03, sigma=0.2, T=1, N=steps, M=simulation_times)
-    engine.simulate_paths()
-    print(steps)
-    print(engine.paths)
-    show_sim_df = pd.DataFrame(np.arange(0, steps),engine.paths)
+    paths = engine.simulate_paths()
+
+    show_sim_df = pd.DataFrame(np.arange(0, steps),paths)
     print(show_sim_df.columns)
 
 with col2:
