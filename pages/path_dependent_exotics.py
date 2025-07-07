48472d88e822d4020b7c33978295679fb12b5b60
KenBitSalt <schen0420@gmail.com>
2025-07-07 22:49:42 +0800

    1

--- pages/path_dependent_exotics.py
+++ pages/path_dependent_exotics.py
@@ -22,21 +22,22 @@ visible_strike = False
 col1, col2 = st.columns(2)
 
 with col1:
-    simulation_times = int(st.slider("Set simulation times: ", 50, 1000, 1))
+    constract_dur_years = int(st.slider("Set contract duration (years): ", 1, 10, 1))
+    simulation_times = int(st.slider("Set simulation times: ", 20, 300, 1))
     steps = int(st.slider("Set simulation steps: ", 12, 365, 1))
     engine = MonteCarloEngine(S0=100, r=0.03, sigma=0.2, T=1, N=steps, M=simulation_times)
     paths = engine.simulate_paths()
     #sims = np.arange(1,len(paths)+1)
-    arr = [str(i) for i in range(0, len(paths)+1)]
+    arr = [str(i) for i in range(1, len(paths)+1)]
     print(len(arr))
     print(len(paths))
-    show_sim_df = pd.DataFrame(paths,columns= arr)
-    st.line_chart(show_sim_df)
+    show_sim_df = pd.DataFrame(paths)
+    st.line_chart(show_sim_df.T)
 
 with col2:
     contract_type = st.radio(
     "Select contract genre: ",
-    [":accumulator[Comedy]", "***FCN***", "***CCN***"],
+    ["***Accumulator***", "***FCN***", "***CCN***","***Phoenix***"],
     captions=[
         "雪球",
         "not done yet",
